da0aa7fe565befd659592a1f22ee79b0ec3c3389
guybloo <Guy.bloomenfeld@mail.huji.ac.il>
2021-01-01 15:24:00 +0200

    register

--- app/src/main/java/com/example/byo/MainActivity.java
+++ app/src/main/java/com/example/byo/MainActivity.java
@@ -12,16 +12,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        mAuth = FirebaseAuth.getInstance();
-//        FirebaseUser currentUser = mAuth.getCurrentUser();
-        EventDB e = new EventDB();
-        Event ev = new Event("1","a","a",1,1,"d","d", null);
-        e.updateItem(ev);
     }
 }
---
+++ app/src/main/java/com/example/byo/Registration.java
@@ -0,0 +1,68 @@
+package com.example.byo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Registration extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private String email;
+    private String password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        findViewById(R.id.btn_enter).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = ((EditText)findViewById(R.id.email_text)).getText().toString();
+                password = ((EditText)findViewById(R.id.pass_text)).getText().toString();
+                signInOrRegister();
+
+            }
+        });
+    }
+
+    private void signInOrRegister(){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Sign In!", Toast.LENGTH_LONG).show();
+//                            progressBar.setVisibility(View.GONE);
+//                            returnResult(false);
+                        } else {
+                            mAuth.createUserWithEmailAndPassword(email, password)
+                                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            if (task.isSuccessful()) {
+                                                Toast.makeText(getApplicationContext(), "Register!", Toast.LENGTH_LONG).show();
+
+                                            } else {
+                                                Toast.makeText(getApplicationContext(), "Fail!", Toast.LENGTH_LONG).show();
+
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                });
+    }
+}
